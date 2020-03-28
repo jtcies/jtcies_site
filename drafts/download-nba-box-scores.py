9c3767b92abd772d5cdc8c0ad65c3bb6370f2362
jtcies <jtcies@gmail.com>
2020-03-27 22:12:00 -0400

    writing

---
+++ drafts/download-nba-box-scores.py
@@ -0,0 +1,22 @@
+from sportsreference.nba.boxscore import Boxscore
+from sportsreference.nba.schedule import Schedule
+from sportsreference.nba.teams import Teams
+import pandas as pd
+
+teams = Teams()
+
+indexes = []
+
+for team in teams:
+	games = team.schedule
+	for game in games:
+		indexes.append(game.boxscore_index)
+
+scores = pd.DataFrame()
+
+for index in indexes:
+	score = Boxscore(index)
+	df = score.dataframe
+	scores = scores.append(df)
+
+scores.to_csv("content/data/nba-boxscores-2020.csv")
